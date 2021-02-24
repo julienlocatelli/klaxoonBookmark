2d002f2527e2ef7a0c5b26f6a0e1ed8d05a6af93
Julien Locatelli <julien.locatelli@fednot.be>
2021-02-24 22:32:10 +0100

    chore(): setup redux with rematch

---
+++ client/src/store/configureStore.ts
@@ -0,0 +1,12 @@
+import { init } from '@rematch/core';
+import { mediaModel } from '../media/media.model'
+
+const media = mediaModel();
+
+const store = init({
+  models: {
+    media
+  }
+});
+
+export default store;
