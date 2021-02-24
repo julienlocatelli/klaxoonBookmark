784b504f3a01235a22a24e44b98d02186904c097
Julien Locatelli <julien.locatelli@fednot.be>
2021-02-24 22:41:41 +0100

    chore(): setup i18n

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,21 +2,26 @@ import React from 'react';
 
 import Box from './components/atom/Box';
 import Text from './components/atom/Text';
+import { useTranslation } from './translate/I18n';
 
-const App: React.FC = () => (
-  <Box flex={1} padding={4}>
-    <Box
-      width={740}
-      padding={3}
-      margin="auto"
-      border="1px solid"
-      borderColor="silverPink"
-      borderRadius={3}
-      bg="light"
-    >
-      <Text color="primary">Bookmark Media</Text>
+const App: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Box flex={1} padding={4}>
+      <Box
+        width={740}
+        padding={3}
+        margin="auto"
+        border="1px solid"
+        borderColor="silverPink"
+        borderRadius={3}
+        bg="light"
+      >
+        <Text color="primary">{t('app:mainTitle')}</Text>
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default App;
